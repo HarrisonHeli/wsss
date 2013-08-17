1d252fa7f33d28a3366e4d1e5ad9ce2e113388e4
Andrew Harrison <harrisonheli@car01.(none)>
2013-08-17 16:37:52 +1000

    More fixes

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     // Look to see if a webslide settings file has been givn on the command line
 
 
-    if (argc == 1)
+    if (argc >= 2)
         {
         w.SettingsFilePath = argv[1];
         }
